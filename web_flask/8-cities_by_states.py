e6f2c7d63f7b154c45e799ea44f4d27d50eb5ea4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-28 05:31:40 -0800

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
+"""Flask web application to display states and cities"""
+
 from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Displays states and their cities."""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+@app.route('/states_and_cities', strict_slashes=False)
+def states_and_cities_list():
+    """Renders a list of states and their cities."""
+    all_states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=all_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown."""
+def close_storage(exc):
+    """Closes the storage after each request."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
